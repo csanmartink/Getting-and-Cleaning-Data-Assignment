e6da5dee05b616683ed46c4ea279957fad70567a
Carolina San Martín <c.g.snmartin@gmail.com>
2020-12-19 22:44:56 -0300

    Final version

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,10 @@
 
+#===============================================================================
+#       Project : Final Assignment course "Getting and Cleaning Data"
+#       Author: Carolina San Martín
+#       Date: 19/12/2020
+#
+#===============================================================================
 
 #########       Getting the data      ==========================================
 # Loading required packages
@@ -44,4 +50,27 @@ tidy_data <- merged_data %>% select(subject, code, contains("mean"), contains("s
 tidy_data$code <- activities[tidy_data$code, 2]
 
 # 4. Appropriately labels the data set with descriptive variable names. 
+names(tidy_data)[2] = "activity"
+names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
+names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
+names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
+names(tidy_data)<-gsub("-mean()", "Mean", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-std()", "STD", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-freq()", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
+names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidy_data_summary <- tidy_data %>%
+        group_by(subject, activity) %>%
+        summarise_all(funs(mean))
+write.table(tidy_data_summary, "tidy_data_summary.txt", row.names = FALSE)
+
+#####   Message confirming the successfully execution   ========================
+message("The script 'run_analysis.R was executed successfully. \n",
+        "Result: a new tidy dataset was created with name \n", 
+        "'tidy_data_summary.txt' in the working directory.")
